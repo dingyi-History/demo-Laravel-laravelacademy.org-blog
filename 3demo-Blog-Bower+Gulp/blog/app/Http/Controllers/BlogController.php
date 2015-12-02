f05598d8e3a3b0979621d1df5296522cbad1a521
丁一鸣 <315129552@qq.com>
2015-12-02 19:58:01 +0800

    demo3

---
+++ 3demo-Blog-Bower+Gulp/blog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::where('published_at', '<=', Carbon::now())
+            ->orderBy('published_at', 'desc')
+            ->paginate(config('blog.posts_per_page'));
+
+        return view('blog.index', compact('posts'));
+    }
+
+    public function showPost($slug)
+    {
+        $post = Post::whereSlug($slug)->firstOrFail();
+        return view('blog.post')->withPost($post);
+    }
+}
---
+++ 3demo-Blog-Bower+Gulp/blog/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Model::unguard();
+        $this->call('PostTableSeeder');
+    }
+
+}
+
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        App\Post::truncate();
+        factory(App\Post::class, 20)->create();
+    }
+}
+
