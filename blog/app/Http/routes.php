7d3c0516f323e9d0b3a82a83ca94b22561cd080a
丁一鸣 <315129552@qq.com>
2015-12-04 13:27:11 +0800

    3 10分钟博客

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+get('/', function () {
+    return redirect('/blog');
 });
+
+get('blog', 'BlogController@index');
+get('blog/{slug}', 'BlogController@showPost');
--- blog/database/seeds/DatabaseSeeder.php
+++ blog/database/seeds/DatabaseSeeder.php
@@ -7,15 +7,20 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
     public function run()
     {
         Model::unguard();
+        $this->call('PostTableSeeder');
+    }
 
-        // $this->call(UserTableSeeder::class);
+}
 
-        Model::reguard();
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        App\Post::truncate();
+        factory(App\Post::class, 20)->create();
     }
 }
